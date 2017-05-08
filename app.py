9f22fc3d85bb120f5b3e81c027959650719137d9
adithya <adithyakreddy6@gmail.com>
2017-05-08 13:11:32 +0530

    Simplified JSON logic

--- app.py
+++ app.py
@@ -19,32 +19,19 @@ except KeyError as e:
     print("Couldn't find the required keys in keys.json.")
     sys.exit(1)
 
-with open('test_images/happy.jpg', 'rb') as image_file:
+with open(sys.argv[1], 'rb') as image_file:
     image = image_file.read()
     r = requests.post(url='https://westus.api.cognitive.microsoft.com/emotion/v1.0/recognize',
                       headers=microsoft_emotion_headers,
                       data=image)
 
 emotion_data = json.loads(r.text)
+emotions = dict(emotion_data[0]['scores'])
 
-emotions = dict()
-emotions['anger'] = emotion_data[0]['scores']['anger']
-emotions['contempt'] = emotion_data[0]['scores']['contempt']
-emotions['neutral'] = emotion_data[0]['scores']['neutral']
-emotions['sadness'] = emotion_data[0]['scores']['sadness']
-emotions['anger'] = emotion_data[0]['scores']['anger']
-emotions['happiness'] = emotion_data[0]['scores']['happiness']
-emotions['disgust'] = emotion_data[0]['scores']['disgust']
-emotions['surprise'] = emotion_data[0]['scores']['surprise']
-emotions['fear'] = emotion_data[0]['scores']['fear']
+keys = list(emotions.keys())
+vals = list(emotions.values())
 
+dominant_emotion = keys[vals.index(max(vals))]
+certainty_score = max(vals)
 
-def get_dominant_emotion(d):
-    d = dict(d)
-    vals = list(d.values())
-    keys = list(d.keys())
-
-    return (keys[vals.index(max(vals))], max(vals))
-
-
-print(get_dominant_emotion(emotions))
+print(dominant_emotion, certainty_score)
